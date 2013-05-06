c69484d4a48c842e6777516b7c41c36bcd95eaba
David Osborn <davidcosborn@gmail.com>
2013-05-06 10:34:27 -0600

    I decided the stuff in <script> tags should be indented.

--- include/html/html-head-script.php
+++ include/html/html-head-script.php
@@ -1,10 +1,10 @@
 <!-- LESS configuration -->
 <script>
-less =
-{
-	env: 'development', // 'production'
-	rootpath: <?php echo SITE_ROOT_URL?>
-};
+	less =
+	{
+		env: 'development', // 'production'
+		rootpath: <?php echo SITE_ROOT_URL?>
+	};
 </script>
 
 <!-- auto-load scripts -->
@@ -13,16 +13,16 @@ less =
 
 <!-- SEO by Amortech -->
 <script>
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-29436860-1']);
-_gaq.push(['_trackPageview']);
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-29436860-1']);
+	_gaq.push(['_trackPageview']);
 
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
+	(function() {
+		var ga = document.createElement('script');
+		ga.type = 'text/javascript';
+		ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
+	})();
 </script>
